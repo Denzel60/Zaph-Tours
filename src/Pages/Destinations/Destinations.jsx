980c8bd78bf2d5a59540372734b9d6a1f7e245d5
Denzel <lawrencedenzel60@gmail.com>
2024-06-09 14:11:34 +0300

    "Updated Destinations page with new CSS styles and JSX components, removed unused image assets, and modified Trip page CSS."

--- src/Pages/Destinations/Destinations.jsx
+++ src/Pages/Destinations/Destinations.jsx
@@ -1,6 +1,61 @@
+import './Destinations.css'
+import mtKenya from '../../assets/MtKenya.jpg'
+
 const Destinations = () => {
     return (
-        <div><h1>Destinations</h1></div>
+        <div>
+            <section className="hero-dest">
+                <h1>#Destinations</h1>
+                <p>Get Started</p>
+            </section>
+
+            <section className="destination-cards">
+                <div className="destinations">
+                    <img src={mtKenya} alt="" />
+                    <button type="button">Book Now</button>
+                    <div className="destination-text">
+                        <h3>Mount Kenya</h3>
+                        <p><span>Description</span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde praesentium inventore excepturi voluptatem voluptatum optio. Ipsa, dignissimos consequuntur! Reprehenderit, optio?</p>
+                        <p>Price Per Group:</p>
+                        <p>Price Per Group:</p>
+                    </div>
+                </div>
+
+                <div className="destinations">
+                    <img src={mtKenya} alt="" />
+                    <button type="button">Book Now</button>
+                    <div className="destination-text">
+                        <h3>Mount Kenya</h3>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde praesentium inventore excepturi voluptatem voluptatum optio. Ipsa, dignissimos consequuntur! Reprehenderit, optio?</p>
+                        <p>Price Per Group:</p>
+                        <p>Price Per Group:</p>
+                    </div>
+                </div>
+
+                <div className="destinations">
+                    <img src={mtKenya} alt="" />
+                    <button type="button">Book Now</button>
+                    <div className="destination-text">
+                        <h3>Mount Kenya</h3>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde praesentium inventore excepturi voluptatem voluptatum optio. Ipsa, dignissimos consequuntur! Reprehenderit, optio?</p>
+                        <p>Price Per Group:</p>
+                        <p>Price Per Group:</p>
+                    </div>
+                </div>
+
+                <div className="destinations">
+                    <img src={mtKenya} alt="" />
+                    <button type="button">Book Now</button>
+                    <div className="destination-text">
+                        <h3>Mount Kenya</h3>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde praesentium inventore excepturi voluptatem voluptatum optio. Ipsa, dignissimos consequuntur! Reprehenderit, optio?</p>
+                        <p>Price Per Group:</p>
+                        <p>Price Per Group:</p>
+                    </div>
+                </div>
+
+            </section>
+        </div >
     )
 }
 
