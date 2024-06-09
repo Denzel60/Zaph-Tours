c248343c566016adc9984843d2cac98ff078e781
Denzel <lawrencedenzel60@gmail.com>
2024-06-09 12:03:17 +0300

    "Updated Destinations page CSS and JSX, added new styles and components, and modified existing ones."

--- src/Pages/Destinations/Destinations.jsx
+++ src/Pages/Destinations/Destinations.jsx
@@ -1,9 +1,136 @@
 import './Destinations.css'
+import mtKenya from '../../assets/MtKenya.jpg'
+import { FaLocationDot } from "react-icons/fa6";
+import { FaStar } from "react-icons/fa";
 
 const Destinations = () => {
     return (
         <div>
             <section className="hero-dest">
+                <h1>#Destinations</h1>
+                <p>Get Started</p>
+            </section>
+
+            <section className="destination-cards">
+                <div className="destinations">
+                    <div className="destination-img">
+                        <img src={mtKenya} alt="" />
+                    </div>
+                    <div className="destination-description">
+                        <h2>Mount Kenya</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore deserunt quae ullam maxime minima ducimus aliquid tempora quos ea accusantium.</p>
+                        <div className="destination-location">
+                            <button><FaLocationDot />      Kenya</button>
+                        </div>
+                    </div>
+                    <div className="destination-cost">
+                        <p>Reviews:  <FaStar /><FaStar /><FaStar /><FaStar /><FaStar /></p>
+                        <h3>Ksh 100000 </h3>
+                        <p>/group of 6</p> <h3> Ksh 20000  </h3>
+                        <p>/person</p>
+
+                    </div>
+                </div>
+
+                <div className="destinations">
+                    <div className="destination-img">
+                        <img src={mtKenya} alt="" />
+                    </div>
+                    <div className="destination-description">
+                        <h2>Mount Kenya</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore deserunt quae ullam maxime minima ducimus aliquid tempora quos ea accusantium.</p>
+                        <div className="destination-location">
+                            <button><FaLocationDot />      Kenya</button>
+                        </div>
+                    </div>
+                    <div className="destination-cost">
+                        <p>Reviews:  <FaStar /><FaStar /><FaStar /><FaStar /><FaStar /></p>
+                        <h3>Ksh 100000 </h3>
+                        <p>/group of 6</p> <h3> Ksh 20000  </h3>
+                        <p>/person</p>
+
+                    </div>
+                </div>
+
+                <div className="destinations">
+                    <div className="destination-img">
+                        <img src={mtKenya} alt="" />
+                    </div>
+                    <div className="destination-description">
+                        <h2>Mount Kenya</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore deserunt quae ullam maxime minima ducimus aliquid tempora quos ea accusantium.</p>
+                        <div className="destination-location">
+                            <button><FaLocationDot />      Kenya</button>
+                        </div>
+                    </div>
+                    <div className="destination-cost">
+                        <p>Reviews:  <FaStar /><FaStar /><FaStar /><FaStar /><FaStar /></p>
+                        <h3>Ksh 100000 </h3>
+                        <p>/group of 6</p> <h3> Ksh 20000  </h3>
+                        <p>/person</p>
+
+                    </div>
+                </div>
+
+                <div className="destinations">
+                    <div className="destination-img">
+                        <img src={mtKenya} alt="" />
+                    </div>
+                    <div className="destination-description">
+                        <h2>Mount Kenya</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore deserunt quae ullam maxime minima ducimus aliquid tempora quos ea accusantium.</p>
+                        <div className="destination-location">
+                            <button><FaLocationDot />      Kenya</button>
+                        </div>
+                    </div>
+                    <div className="destination-cost">
+                        <p>Reviews:  <FaStar /><FaStar /><FaStar /><FaStar /><FaStar /></p>
+                        <h3>Ksh 100000 </h3>
+                        <p>/group of 6</p> <h3> Ksh 20000  </h3>
+                        <p>/person</p>
+
+                    </div>
+                </div>
+
+                <div className="destinations">
+                    <div className="destination-img">
+                        <img src={mtKenya} alt="" />
+                    </div>
+                    <div className="destination-description">
+                        <h2>Mount Kenya</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore deserunt quae ullam maxime minima ducimus aliquid tempora quos ea accusantium.</p>
+                        <div className="destination-location">
+                            <button><FaLocationDot />      Kenya</button>
+                        </div>
+                    </div>
+                    <div className="destination-cost">
+                        <p>Reviews:  <FaStar /><FaStar /><FaStar /><FaStar /><FaStar /></p>
+                        <h3>Ksh 100000 </h3>
+                        <p>/group of 6</p> <h3> Ksh 20000  </h3>
+                        <p>/person</p>
+
+                    </div>
+                </div>
+
+                <div className="destinations">
+                    <div className="destination-img">
+                        <img src={mtKenya} alt="" />
+                    </div>
+                    <div className="destination-description">
+                        <h2>Mount Kenya</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore deserunt quae ullam maxime minima ducimus aliquid tempora quos ea accusantium.</p>
+                        <div className="destination-location">
+                            <button><FaLocationDot />      Kenya</button>
+                        </div>
+                    </div>
+                    <div className="destination-cost">
+                        <p>Reviews:  <FaStar /><FaStar /><FaStar /><FaStar /><FaStar /></p>
+                        <h3>Ksh 100000 </h3>
+                        <p>/group of 6</p> <h3> Ksh 20000  </h3>
+                        <p>/person</p>
+
+                    </div>
+                </div>
             </section>
         </div>
     )
